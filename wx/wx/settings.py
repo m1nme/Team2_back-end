14d9edf41b0febf33c799d59c8aeca0d57a0e917
M1N <70762934+m1nme@users.noreply.github.com>
2020-11-14 00:04:20 +0800

    Update settings.py

--- wx/wx/settings.py
+++ wx/wx/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_3k80&#t)j1*xbsf*bnha+wu6axueep+(us*-vvc@=d(vqf88$'
+SECRET_KEY = 'dasdadsadasdsadsadasdasdsadas'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
